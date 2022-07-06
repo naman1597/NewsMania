043a1f1149f55d1b56d3378846893973961bde9b
naman1597 <singhnaman996@gmail.com>
2022-07-06 20:47:13 +0530

    Hide Api Key

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import LoadingBar from 'react-top-loading-bar';
 
 const App = ()=> {
   //const pageSize = 5;
-  const apiKey = 'pub_8978cd7bcc704eea55ad227670a12c62eebd';
+  const apiKey = process.env.REACT_APP_NEWS_API;
   const [progress, setProgress] = useState(0)
  
     return (
